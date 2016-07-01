c880f50e32de06d78711edd0ae0030bfbb43e7d4
Jin Qing <jinq0123@163.com>
2016-07-01 17:12:50 +0800

    Change test.

--- test/main.lua
+++ test/main.lua
@@ -1,4 +1,6 @@
 package.path = "../?.lua;" .. package.path
+local hotfix = require("hotfix")
+local test  -- = require("test")
 
 local function log(msg)
     local f = assert(io.open("log.txt", "a+"))
@@ -6,56 +8,59 @@ local function log(msg)
     assert(f:close())
 end  -- log
 
-local hotfix = require("hotfix")
+local function write_test_lua(s)
+    local f = assert(io.open("test.lua", "w"))
+    f:write(s)
+    assert(f:close())
+end  -- write_test_lua()
+
+local function run_test(old, new, check)
+    assert("string" == type(old))
+    assert("string" == type(new))
+    assert("function" == type(check))
+    write_test_lua(old);
+    package.loaded["test"] = nil
+    test = require("test")
+    write_test_lua(new)
+    test = hotfix.hotfix_module("test")
+    check()
+end  -- run_test()
+
 hotfix.log_error = log
 hotfix.log_info = log
 hotfix.log_debug = log
 log("--------------------")
-
-local TEST = "test.lua"
-
-local function write_test(s)
-    local f = assert(io.open(TEST, "w"))
-    f:write(s)
-    assert(f:close())
-end  -- write_test()
-
-write_test("")
-local test = require("test")
-
 log("Test keeping upvalue data.")
-write_test([[
-local a = 1
-function get_a()
-    return a
-end
-]])
-test = hotfix.hotfix_module("test")
-assert(1 == get_a())
-write_test([[
-local a = 2
-function get_a()
-    return a
-end
-]])
-test = hotfix.hotfix_module("test")
-assert(1 == get_a())
+run_test([[
+        local a = 1
+        function get_a()
+            return a
+        end
+    ]],
+    [[
+        local a = 2
+        function get_a()
+            return a
+        end
+    ]],
+    function()
+        assert(1 == get_a())
+    end)
 
 log("Test adding functions.")
-write_test([[
-local M = {}
-return M
-]])
-test = hotfix.hotfix_module("test")
-
-write_test([[
-local M = {}
-function g_foo() return 123 end
-function M.foo() return 1234 end
-return M
-]])
-test = hotfix.hotfix_module("test")
-assert(123 == g_foo())
-assert(1234 == test.foo())
+run_test([[
+        local M = {}
+        return M
+    ]],
+    [[
+        local M = {}
+        function g_foo() return 123 end
+        function M.foo() return 1234 end
+        return M
+    ]],
+    function()
+        assert(123 == g_foo())
+        assert(1234 == test.foo())
+    end)
 
 print("Test OK!")
