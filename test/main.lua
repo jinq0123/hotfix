d79b5e5720ae94186f14c26554047e343cbffa14
Jin Qing <jinq0123@163.com>
2016-07-01 18:32:11 +0800

    Add test.

--- test/main.lua
+++ test/main.lua
@@ -1,6 +1,7 @@
 package.path = "../?.lua;" .. package.path
 local hotfix = require("hotfix")
 local test  -- = require("test")
+local tmp = {}  -- to store temp data
 
 local function log(msg)
     local f = assert(io.open("log.txt", "a+"))
@@ -14,30 +15,33 @@ local function write_test_lua(s)
     assert(f:close())
 end  -- write_test_lua()
 
-local function run_test(old, new, check)
+local function run_test(old, prepare, new, check)
     assert("string" == type(old))
+    assert(not prepare or "function" == type(prepare))
     assert("string" == type(new))
-    assert("function" == type(check))
+    assert(not check or "function" == type(check))
     write_test_lua(old);
     package.loaded["test"] = nil
     test = require("test")
+    if prepare then prepare() end
     write_test_lua(new)
     test = hotfix.hotfix_module("test")
-    check()
+    if chekc then check() end
 end  -- run_test()
 
 hotfix.log_error = log
 hotfix.log_info = log
 hotfix.log_debug = log
+
 log("--------------------")
+
 log("Test keeping upvalue data...")
 run_test([[
         local a = 1
         function get_a()
             return a
         end
-    ]],
-    [[
+    ]], nil, [[
         local a = 2
         function get_a()
             return a
@@ -51,8 +55,7 @@ log("Test adding functions...")
 run_test([[
         local M = {}
         return M
-    ]],
-    [[
+    ]], nil, [[
         local M = {}
         function g_foo() return 123 end
         function M.foo() return 1234 end
@@ -61,6 +64,33 @@ run_test([[
     function()
         assert(123 == g_foo())
         assert(1234 == test.foo())
+    end)
+
+log("Hot fix function module...")  -- Module returns a function.
+run_test(
+    "return function() return 12345 end",
+    function() tmp.f = test end,
+    "return function() return 56789 end",
+    function()
+        assert(56789 == test())
+        assert(56789 == tmp.f())
+    end)
+
+log("Test function table...")
+run_test([[
+        local M = {}
+        function M.foo() return 12345 end
+        return M
+    ]],
+    function() tmp.foo = test.foo end,
+    [[
+        local M = {}
+        function M.foo() return 67890 end
+        return M
+    ]],
+    function()
+        assert(67890 == test.foo())
+        assert(67890 == tmp.foo())
     end)
 
 print("Test OK!")
